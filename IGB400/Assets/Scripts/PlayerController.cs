a4a325b3955de7d3502208f18c1f9cc4afc9cb3c
Dracico <szymanski.jim@icloud.com>
2018-09-25 09:56:21 +1000

    First uup

---
+++ IGB400/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 15;
+    private Rigidbody rig;
+
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        
+        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+
+
+        Vector3 movement = transform.forward * speed * Time.deltaTime;
+
+        if (Input.GetKey("z"))
+        {
+            rig.MovePosition(transform.position + movement);
+        }
+        
+
+    }
+}
---
+++ IGB400/Assets/Scripts/Shooter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    public GameObject player;
+    private Rigidbody rigidbodyPlayer;
+    public GameObject bullet;
+    public GameObject aimImage;
+    public bool isInSight = false;
+    public bool shot = false;
+    public GameObject instance;
+    public float directionCoef;
+    
+	// Use this for initialization
+	void Start () {
+        rigidbodyPlayer = player.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isInSight = InSight();
+	}
+
+    //Check if the player is in sight
+    bool InSight()
+    {
+       
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+        RaycastHit hit;
+        var rayDirection = player.transform.position - transform.position;
+        if (Physics.Raycast(transform.position, rayDirection,out hit))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                if (shot == false)
+                {
+                    instance = Instantiate(aimImage, hit.transform.position+(Direction()*directionCoef), aimImage.transform.rotation);
+                    
+                }
+                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } else
+        {
+            return false;
+        }
+    }
+
+    Vector3 Direction()
+    {
+        Vector3 playerDirection = rigidbodyPlayer.velocity;
+        playerDirection.Normalize();
+        return playerDirection;
+    }
+}
